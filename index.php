00a0f49c8524e6a8e36e606b8a9dbe076a7bf4e4
kksanderr <manga978@gmail.com>
2020-06-04 16:25:28 +0200

    prvi commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>ARTHAUS KINO</title>
+     <link href="https://fonts.googleapis.com/css2?family=Fanwood+Text&display=swap"
+     rel="stylesheet">
+     <link rel="stylesheet" href="./css/main.css">
+   </head>
+   <body>
+
+     <!-- Header -->
+     <?php include('./templates/header.php'); ?>
+     <!-- End Header -->
+
+   </body>
+ </html>
---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<header class="header">
+  <ul class="logo">
+    <li> <a href="index.php"><img src="./img/logo.png" alt="Arthaus Kino"></a></li>
+  </ul>
+
+  <ul class="header-menu">
+    <li><a href="about.php">O nama</a></li>
+    <li><a href="calendar.php">Kalendar</a></li>
+    <li><a href="news.php">Vesti</a></li>
+    <li><a href="login.php">Prijavljivanje</a></li>
+    <li><a href="register.php">Registracija</a></li>
+  </ul>
+</header>
