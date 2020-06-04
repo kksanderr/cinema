f63f5535198353b603f56da6de5eea17d994bf20
kksanderr <manga978@gmail.com>
2020-06-04 17:28:05 +0200

    sredjivanje headera

--- templates/header.php
+++ templates/header.php
@@ -4,10 +4,15 @@
   </ul>
 
   <ul class="header-menu">
+    <li class="divider"></li>
     <li><a href="about.php">O nama</a></li>
+    <li class="divider"></li>
     <li><a href="calendar.php">Kalendar</a></li>
+    <li class="divider"></li>
     <li><a href="news.php">Vesti</a></li>
+    <li class="divider"></li>
     <li><a href="login.php">Prijavljivanje</a></li>
+    <li class="divider"></li>
     <li><a href="register.php">Registracija</a></li>
   </ul>
 </header>
