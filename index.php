f022c4d9bf114a249576bdd02d3031ca7af76305
kksanderr <manga978@gmail.com>
2020-06-06 12:36:10 +0200

    ubacen main carousel

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
      <link href="https://fonts.googleapis.com/css2?family=Fanwood+Text&display=swap"
      rel="stylesheet">
      <link rel="stylesheet" href="./css/main.css">
+     <script type="text/javascript" src="./jquery/jquery-3.5.1.min.js"></script>
+     <script type="text/javascript" src="./jquery/jquery.jcarousel.min.js"></script>
+
+     <script type="text/javascript" src="./jquery/jcarousel.basic.js"></script>
    </head>
    <body>
 
@@ -19,5 +23,23 @@
      <?php include('./templates/header.php'); ?>
      <!-- End Header -->
 
+     <!-- Main Carousel -->
+     <div class="main-carousel-wrapper">
+      <div class="main-carousel">
+        <ul>
+         <li> <img src="./img/img1.jpg"> </li>
+         <li> <img src="./img/img2.jpg"> </li>
+         <li> <img src="./img/img3.jpg"> </li>
+        </ul>
+      </div>
+    </div>
+
+    <a href="#" class="main-carousel-control-prev">&lsaquo;</a>
+    <a href="#" class="main-carousel-control-next">&rsaquo;</a>
+
+    <p class="main-carousel-pagination"></p>
+
+     <!-- End Main Carousel -->
    </body>
+
  </html>
---
+++ jquery/jcarousel.basic.js
@@ -0,0 +1,36 @@
+(function($) {
+    $(function() {
+        $('.main-carousel').jcarousel();
+
+        $('.main-carousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.main-carousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.main-carousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})(jQuery);
